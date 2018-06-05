f51ed4833587dacde67378a58001a2a1f5106485
royl8 <royl8@outlook.com>
2018-06-05 22:45:46 +0800

    fix bug: `/api/users/pk` always returns the logged-in user JSON

--- variora/home/api/urls.py
+++ variora/home/api/urls.py
@@ -2,13 +2,13 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'user', views.get_current_user),
+    url(r'user$', views.get_current_user),
 
     url(r'users/(?P<pk>\d+)', views.UserAPIView.as_view()),
-    
+
     url(r'search', views.search_api_view),
-    
+
     url(r'signin', views.sign_in),
-    
+
     url(r'signoff', views.sign_off),
 ]
