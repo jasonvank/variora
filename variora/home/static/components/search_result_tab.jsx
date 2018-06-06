caef682ae783272a5ee06f513db7a813fb5793f1
Jason <fwenkai.jason@gmail.com>
2018-06-06 20:05:24 +0800

    test files for search page

---
+++ variora/home/static/components/search_result_tab.jsx
@@ -0,0 +1,178 @@
+import 'antd/dist/antd.css';
+import 'regenerator-runtime/runtime';
+
+import { Avatar, Button, Col, Icon, Input, Layout, LocaleProvider, Menu, Modal, Row, Upload, Table } from 'antd';
+import {
+  Link,
+  Route,
+  BrowserRouter as Router
+} from 'react-router-dom'
+import { getCookie, getUrlFormat } from 'util.js'
+
+import { GroupDocumentsList } from './group_documents_list.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios'
+import enUS from 'antd/lib/locale-provider/en_US';
+
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
+const MenuItemGroup = Menu.ItemGroup;
+
+
+class SearchResultTab extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      resultDocuments: undefined,
+      resultCoteries: undefined,
+      resultUsers: undefined
+    }
+  }
+
+  componentDidMount() {
+    axios.get('/api/search/?key=j').then((response) => {
+      var data =  response.data;
+      console.log(data);
+      this.setState({
+        resultDocuments: data.resultDocuments,
+        resultCoteries: data.resultCoteries,
+        resultUsers: data.resultUsers
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div style={{ paddingLeft: 18, paddingRight: 18, paddingTop: 8, margin: 0, minHeight: 280 }}> 
+        <div style={{ overflow: 'auto', backgroundColor: 'white', marginTop: 18, boxShadow: '2px 3px 8px rgba(0, 0, 0, .25)' }}>
+          <DocumentResult documentResult={this.state.resultDocuments} />
+        </div>
+        <div style={{ overflow: 'auto', backgroundColor: 'white', marginTop: 18, boxShadow: '2px 3px 8px rgba(0, 0, 0, .25)' }}>
+          <GroupResult groupResult={this.state.resultCoteries} />
+        </div>
+        <div style={{ overflow: 'auto', backgroundColor: 'white', marginTop: 18, boxShadow: '2px 3px 8px rgba(0, 0, 0, .25)' }}>
+          <UserResult userResult={this.state.resultUsers} />
+        </div> 
+      </div>
+    )
+  }
+}
+
+
+class DocumentResult extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: this.props.resultDocuments,
+      columns: [{
+        title: 'id',
+        dataIndex: 'id',
+      }, {
+        title: 'Document Name',
+        dataIndex: 'documentName',
+      }]
+    }
+  }
+
+  async componentWillReceiveProps(nextProps) {
+    await this.setState({
+      data: nextProps.resultDocuments
+    })
+    this.forceUpdate()
+  }
+
+  render() {
+    return (
+      <Table
+        dataSource={this.state.data}
+        columns={this.state.columns}
+        pagination={false}
+      />
+    )
+  }
+}
+
+
+class GroupResult extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: this.props.resultCoteries,
+      columns: [{
+
+        title: 'id',
+        dataIndex: 'id',
+      }, {
+        title: 'Group Name',
+        dataIndex: 'groupName',
+      }]
+    }
+  }
+
+  async componentWillReceiveProps(nextProps) {
+    await this.setState({
+      data: nextProps.resultCoteries
+    })
+    this.forceUpdate()
+  }
+
+  render() {
+    return (
+      <Table
+        dataSource={this.state.data}
+        columns={this.state.columns}
+        pagination={false}
+      />
+    )
+  }
+}
+
+class UserResult extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: this.props.resultUsers,
+      columns: [{
+        title: 'id',
+        dataIndex: 'id',
+      }, {
+        title: 'User Name',
+        dataIndex: 'userName',
+      }]
+    }
+  }
+
+  async componentWillReceiveProps(nextProps) {
+    await this.setState({
+      data: nextProps.resultUsers
+    })
+    this.forceUpdate()
+  }
+
+  render() {
+    return (
+      <Table
+        dataSource={this.state.data}
+        columns={this.state.columns}
+        pagination={false}
+      />
+    )
+  }
+}
+
+
+
+
+
+export { SearchResultTab }
+
+
+
+
+
+
+
+
+
+
