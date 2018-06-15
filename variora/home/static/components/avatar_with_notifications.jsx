e049141bf4bd79938ec863f8c735b86fee645d95
Jason <fwenkai.jason@gmail.com>
2018-06-15 12:13:55 +0800

    Display group name and messages on invitee's notifcation board.

--- variora/home/static/components/avatar_with_notifications.jsx
+++ variora/home/static/components/avatar_with_notifications.jsx
@@ -7,30 +7,68 @@ import { getCookie, getUrlFormat } from 'util.js'
 import React from 'react';
 import axios from 'axios'
 
+
+
+class InvitationDetailsWrapper extends React.Component{
+  render() {
+    var message = this.props.invitation
+    var messageListItems =
+      <p>Invitation from Group: {message.coterie_name}
+        <br />
+        { message.invitation_message }
+      </p>
+
+    return (
+      <div>
+        {messageListItems}
+      </div>
+    )
+  }
+}
+
 class ReceivedCoterieInvitationNotificationContent extends React.Component {
   constructor(props) {
     super(props)
     this.onAcceptClick = this.onAcceptClick.bind(this)
     this.onRejectClick = this.onRejectClick.bind(this)
+    // this.invitationDetails = this.invitationDetails.bind(this)
   }
 
+  // invitationDetails() {
+  //   var messageDetails = this.props.invitation
+  //   console.log(messageDetails)
+  //   this.setState ({
+  //     message :
+  //     <p>Invitation from Group: {messageDetails.coterie_name}
+  //       <br />
+  //       { messageDetails.invitation_message }
+  //     </p>
+  //   })
+  // }
+
   onAcceptClick() {
+    console.log("Accept post: ", this.props.invitation.accept_url),
     notification.close(this.props.invitation.pk)
   }
 
   onRejectClick() {
+    console.log("Reject post: ", this.props.invitation.reject_url),
     notification.close(this.props.invitation.pk)
   }
 
   render() {
     return (
       <div>
-        <Button style={{ margin: '12px 8px 6px 8px' }} type="primary" size="small" onClick={this.onAcceptClick}>
-          Accept
-        </Button>
-        <Button style={{ margin: '12px 8px 6px 8px' }} type="primary" size="small" onClick={this.onRejectClick}>
-          Reject
-        </Button>
+      <InvitationDetailsWrapper invitation={this.props.invitation}/>
+        {/* <p>Invitation from Group: {this.props.invitation.coterie_name} <br /> { this.props.invitation.invitation_message }</p> */}
+        <div>
+          <Button style={{ margin: '12px 8px 6px 8px' }} type="primary" size="small" onClick={this.onAcceptClick}>
+            Accept
+          </Button>
+          <Button style={{ margin: '12px 8px 6px 8px' }} type="primary" size="small" onClick={this.onRejectClick}>
+            Reject
+          </Button>
+        </div>
       </div>
     )
   }
