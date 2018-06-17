0a7f156be4bf047fac16e4c5709e3b28e8e5be0f
royl8 <royl8@outlook.com>
2018-06-17 17:10:36 +0800

    refacotring

--- variora/coterie/api/urls.py
+++ variora/coterie/api/urls.py
@@ -16,13 +16,13 @@ urlpatterns = [
 
     url(r'coteries/(?P<pk>\d+)$', views.CoterieView.as_view()),
 
-    url(r'coteries/(?P<pk>\d+)/(?P<operation>\w+)', views.CoterieView.as_view()),
+    url(r'coteries/(?P<pk>\d+)/(?P<operation>\w+)$', views.CoterieView.as_view()),
 
     url(r'^coteries/create$', views.create_coterie),
 
     url(r'^coteriedocuments/(?P<pk>\d+)$', views.CoterieDocumentView.as_view()),
 
-    url(r'^coteriedocuments/(?P<pk>\d+)/(?P<operation>\w+)', views.CoterieDocumentView.as_view()),
+    url(r'^coteriedocuments/(?P<pk>\d+)/(?P<operation>\w+)$', views.CoterieDocumentView.as_view()),
 ]
 
 
