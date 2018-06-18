f263d89f94ba4d08218e8883a6366135ee779a3d
Jason <fwenkai.jason@gmail.com>
2018-06-18 15:32:00 +0800

    Rename exitGroupCallback to removeMemberCallback

--- variora/home/static/components/group_members_subtab.jsx
+++ variora/home/static/components/group_members_subtab.jsx
@@ -84,7 +84,7 @@ class GroupMembersList extends React.Component {
   render() {
     function DropdownWrapper(props) {
       var menu = (
-        <Menu onClick={ () => props.exitGroupCallback(props.memberEmailAddress) }>
+        <Menu onClick={ () => props.removeMemberCallback(props.memberEmailAddress) }>
           <Menu.Item>Remove</Menu.Item>
         </Menu>
       );
@@ -119,7 +119,7 @@ class GroupMembersList extends React.Component {
       key: 'action',
       width: "20%",
       render: (text, record) => (
-        <DropdownWrapper exitGroupCallback={this.props.exitGroupCallback} memberEmailAddress={ record.email_address } />
+        <DropdownWrapper removeMemberCallback={this.props.removeMemberCallback} memberEmailAddress={ record.email_address } />
       ),
     }]
 
@@ -251,7 +251,7 @@ class GroupMembersSubtab extends React.Component {
       })
       .catch(e => { message.warning(e.message) })
     }
-    this.exitGroupCallback = (memberEmailAddress) => {
+    this.removeMemberCallback = (memberEmailAddress) => {
       var self = this
       var data = new FormData()
       data.append('csrfmiddlewaretoken', getCookie('csrftoken'))
@@ -285,7 +285,7 @@ class GroupMembersSubtab extends React.Component {
           <GroupAdministratorsList coteriePk={this.state.coteriePk} administrators={this.state.administrators} />
         </div>
         <div style={{ overflow: 'auto', backgroundColor: 'white', marginTop: 18, boxShadow: '2px 3px 8px rgba(0, 0, 0, .20)' }}>
-          <GroupMembersList coteriePk={this.state.coteriePk} members={this.state.members} exitGroupCallback={this.exitGroupCallback} />
+          <GroupMembersList coteriePk={this.state.coteriePk} members={this.state.members} removeMemberCallback={this.removeMemberCallback} />
         </div>
         <div style={{ overflow: 'auto', backgroundColor: 'white', marginTop: 18, boxShadow: '2px 3px 8px rgba(0, 0, 0, .20)' }}>
           <GroupApplicationList coteriePk={this.state.coteriePk} applications={this.state.applications} updateApplicationListCallback={this.updateApplicationListCallback}/>
