1511622cb5b3f254d4471cd94e78229a7e0de1ef
royl8 <royl8@outlook.com>
2018-06-19 19:58:36 +0800

    install python dependencies during deployment

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 source env/bin/activate
 
+pip install -r prod_reqs
+
 python variora/manage.py makemigrations
 python variora/manage.py migrate
 sudo env/bin/python variora/manage.py collectstatic --noinput
