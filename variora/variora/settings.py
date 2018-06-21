9a7ee31d529c0ab1423663aa9d0a14c2d5faeb6b
royl8 <royl8@outlook.com>
2018-06-21 21:36:02 +0800

    move database settings into private file

--- variora/variora/settings.py
+++ variora/variora/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-from private_settings import DEBUG, SECRET_KEY
+from private_settings import DEBUG, SECRET_KEY, DATABASES
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -68,16 +68,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'variora.wsgi.application'
-
-# Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 CACHES = {
     'default': {
