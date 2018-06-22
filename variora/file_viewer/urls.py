b897e1c6ece92d961e89b5c9f9f4dd831afcb786
royl8 <royl8@outlook.com>
2018-06-22 23:03:40 +0800

    more flexible char set for document naming

--- variora/file_viewer/urls.py
+++ variora/file_viewer/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.FileViewerView.as_view(), name="file_viewer"),
-    url(r'^(?P<title>[\w|\-|&]*)/(?P<pk>\d+)', views.FileViewerView.as_view(), name="file_viewer"),
+    url(r'^(?P<title>[\w|\-|&|.|(|)|:|[|\]|@|<|>]*)/(?P<pk>\d+)', views.FileViewerView.as_view(), name="file_viewer"),
 
     url(r'^api/', include('file_viewer.api.urls')),
 ]
