bb32b8fd4c5f342c4f2b97eefb45bf0edb637f26
royl8 <royl8@outlook.com>
2018-06-22 23:30:25 +0800

    fix test

--- jest_tests/test.test.js
+++ jest_tests/test.test.js
@@ -1,15 +1,21 @@
 import { DocumentTab } from '../variora/home/static/components/document_tab.jsx'
 import React from 'react';
+import { StaticRouter } from 'react-router'
 import renderer from 'react-test-renderer';
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3);
-});
+  expect(1 + 2).toBe(3)
+})
+
+const location = {
+  'pathname': '/'
+}
 
 test('test react', () => {
   const component = renderer.create(
-    <DocumentTab />,
-  ).toJSON();
+    <StaticRouter location={location} context={{}}>
+      <DocumentTab location={location} />
+    </StaticRouter>
+  ).toJSON()
   expect(component).toMatchSnapshot()
-});
-
+})
