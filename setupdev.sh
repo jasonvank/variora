f15aa191aa7119820958ac9884bba4a1c9f6abdd
royl8 <royl8@qq.com>
2018-07-01 08:04:44 +0800

    .

--- setupdev.sh
+++ setupdev.sh
@@ -1,8 +1,11 @@
-pip install --upgrade pip
+sudo pip install --upgrade pip
 npm i -g npm
 
 npm install webpack yarn -g
-
 yarn install
+
+sudo pip install virtualenv
+virtualenv env
+./env/bin/activate
 pip install -r dev_reqs.txt
 
