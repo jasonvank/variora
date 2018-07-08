ed121f7864a25dcc598de8013dc12204957d2cd9
royl8 <royl8@qq.com>
2018-07-08 11:21:17 +0800

    refactoring

--- variora/file_viewer/static/navbar_subpage_script.js
+++ variora/file_viewer/static/navbar_subpage_script.js
@@ -3,45 +3,44 @@ import { getCookie } from 'util.js'
 function prepareNavbarFunction() {
   // coterie file viewer pages do not have collect_button
   // only normal file viewer pages have
-  $("#collect_button").on("click", function(){
-    var span = $(this).find(".fa")
-    if (span.hasClass("fa-star-o")) {
+  $('#collect_button').on('click', function(){
+    var span = $(this).find('.fa')
+    if (span.hasClass('fa-star-o')) {
       layer.msg('Collected')
-      span.removeClass("fa-star-o");
-      span.addClass("fa-star")
+      span.removeClass('fa-star-o')
+      span.addClass('fa-star')
       $.ajax({
-        type: "POST",
-        url: "",
+        type: 'POST',
+        url: '',
         data: {
           csrfmiddlewaretoken: getCookie('csrftoken'),
-          operation: "collect",
+          operation: 'collect',
           document_id: $("button[name='document_id']").val(),
         },
       })
-    }
-    else if (span.hasClass("fa-star")) {
+    } else if (span.hasClass('fa-star')) {
       layer.msg('Uncollected')
-      span.removeClass("fa-star")
-      span.addClass("fa-star-o")
+      span.removeClass('fa-star')
+      span.addClass('fa-star-o')
       $.ajax({
-        type: "POST",
-        url: "",
+        type: 'POST',
+        url: '',
         data: {
           csrfmiddlewaretoken: getCookie('csrftoken'),
-          operation: "uncollect",
+          operation: 'uncollect',
           document_id: $("button[name='document_id']").val(),
         },
       })
     }
   })
 
-  $("#show_annotation_frame_button").on('click', function() {
-    $(".Annotation").each(function() {
+  $('#show_annotation_frame_button').on('click', function() {
+    $('.Annotation').each(function() {
       $(this).slideDown(180)
     })
   })
-  $("#hide_annotation_frame_button").on('click', function() {
-    $(".Annotation").each(function() {
+  $('#hide_annotation_frame_button').on('click', function() {
+    $('.Annotation').each(function() {
       $(this).slideUp(180)
     })
   })
