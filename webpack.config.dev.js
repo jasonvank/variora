95bfcbe0faec5a088f78614ddc031ccb3bffff0c
royl8 <royl8@qq.com>
2018-07-10 21:39:13 +0800

    update webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,9 +1,9 @@
-var path = require('path');
+var path = require('path')
 
 
 /***** import webpack.config.js as the base configuration *****/
-var prodConfig = require('./webpack.config.js');
-devConfig = prodConfig;
+var prodConfig = require('./webpack.config.js')
+devConfig = prodConfig
 
 
 /**************** modify the following settings: **************/
@@ -11,14 +11,14 @@ devConfig = prodConfig;
 // change the destination path of the bundled javascript files
 // from ./variora/bundled_static/prod/bundle
 // to   ./variora/bundled_static/dev/bundle
-devConfig.output.path = path.resolve('./variora/bundled_static/dev/bundle');
+devConfig.output.path = path.resolve('./variora/bundled_static/dev/bundle')
 
 // set the mode to watch
 // so whenever the files get changed, the bundle will be updated accordingly immediately
 devConfig.watch = true
 devConfig.watchOptions = {
   ignored: /node_modules/,
-  poll: 1000
+  poll: 1800  // every 1.8 seconds, webpack checks file update
 }
 
 module.exports = devConfig
