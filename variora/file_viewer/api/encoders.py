0dd910f2d0f7c6eef0e00199fa46a7faa0c34110
royl8 <royl8@outlook.com>
2018-07-10 11:20:31 +0800

    refactoring

---
+++ variora/file_viewer/api/encoders.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.models import AnonymousUser
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.serializers.json import DjangoJSONEncoder
+
+from home.models import User
+
+from ..models import Annotation, AnnotationReply, Comment, Document, DocumentThumbnail
+
+
+class DocumentEncoder(DjangoJSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Document):
+            return {
+                'pk': obj.pk,
+                'uuid': obj.uuid,
+                'slug': obj.slug,
+                'title': obj.title,
+                'num_visit': str(obj.num_visit),
+                'url': obj.url,
+                'download_method': 'get',
+                'download_url': '/file_viewer/api/documents/' + str(obj.pk) + '/download',
+                'delete_method': 'post',
+                'delete_url': '/file_viewer/api/documents/' + str(obj.pk) + '/delete',
+                'file_on_server': obj.file_on_server,
+                'renameUrl': '/file_viewer/api/documents/' + str(obj.pk) + '/rename',
+                'uncollectUrl': '/file_viewer/api/documents/' + str(obj.pk) + '/uncollect',
+                'upload_time': obj.upload_time,
+            }
+        return super(DocumentEncoder, self).default(obj)
+
+
+class DocumentThumbnailEncoder(DjangoJSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, DocumentThumbnail):
+            return {
+                'title': obj.document.title,
+                'image': obj.thumbnail_image.url,
+                'open_url': '/documents/' + obj.document.slug + '/' + obj.document.title,
+                'description': obj.description
+            }
+        return super(DocumentThumbnailEncoder, self).default(obj)
--- variora/file_viewer/api/views.py
+++ variora/file_viewer/api/views.py
@@ -13,29 +13,7 @@ from django.views.generic import View
 from home.models import User
 
 from ..models import Annotation, AnnotationReply, Comment, Document, DocumentThumbnail
-from variora import utils
-
-
-class DocumentEncoder(DjangoJSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, Document):
-            return {
-                'pk': obj.pk,
-                'uuid': obj.uuid,
-                'slug': obj.slug,
-                'title': obj.title,
-                'num_visit': str(obj.num_visit),
-                'url': obj.url,
-                'download_method': 'get',
-                'download_url': '/file_viewer/api/documents/' + str(obj.pk) + '/download',
-                'delete_method': 'post',
-                'delete_url': '/file_viewer/api/documents/' + str(obj.pk) + '/delete',
-                'file_on_server': obj.file_on_server,
-                'renameUrl': '/file_viewer/api/documents/' + str(obj.pk) + '/rename',
-                'uncollectUrl': '/file_viewer/api/documents/' + str(obj.pk) + '/uncollect',
-                'upload_time': obj.upload_time,
-            }
-        return super(DocumentEncoder, self).default(obj)
+from .encoders import DocumentEncoder, DocumentThumbnailEncoder
 
 
 def _delete_document(document, user):
@@ -129,17 +107,5 @@ class DocumentListView(View):
         )
 
 
-class DocumentThumbnailEncoder(DjangoJSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, DocumentThumbnail):
-            return {
-                'title': obj.document.title,
-                'image': obj.thumbnail_image.url,
-                'open_url': '/documents/' + obj.document.slug + '/' + obj.document.title,
-                'description': obj.description
-            }
-        return super(DocumentThumbnailEncoder, self).default(obj)
-
 def get_top_document_thumbnails(request):
     return JsonResponse(list(DocumentThumbnail.objects.all()), encoder=DocumentThumbnailEncoder, safe=False)
-
