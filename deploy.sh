c3a96760dffe140ebbde1d0d4c5eba24d346b40e
royl8 <royl8@outlook.com>
2018-07-11 11:05:40 +0800

    no need to restart apache every time make deployment. since django app runs in daemon mode, can just touch wsgi entry to ask apache to reload django content

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,6 @@ source env/bin/activate
 
 pip install -r prod_reqs.txt
 
-sudo service apache2 stop
-
 python variora/manage.py makemigrations
 python variora/manage.py migrate
 
@@ -12,6 +10,10 @@ sudo env/bin/python variora/manage.py collectstatic --noinput
 sudo env/bin/python variora/manage.py installtasks
 sudo env/bin/python variora/manage.py showtasks
 
-#sudo service memcached restart
+# invalidate content in memcached
 python variora/manage.py invalidate_cachalot -c default
-sudo service apache2 start
+
+# ask apache to reload Django, this command works because Django runs in daemon mode:
+# https://stackoverflow.com/a/4206134/6159872
+# https://code.google.com/archive/p/modwsgi/wikis/ReloadingSourceCode.wiki
+touch variora/variora/wsgi.py
