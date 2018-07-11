32e87f29637a17019f19c571846c34c46396acce
royl8 <royl8@outlook.com>
2018-07-11 17:01:37 +0800

    fix facebook and google login issue and refactoring

--- variora/home/static/sign_in_index.js
+++ variora/home/static/sign_in_index.js
@@ -18,10 +18,68 @@ const Search = Input.Search
 
 
 class NormalLoginForm extends React.Component {
-  onSuccess(user) {
-    console.log(user)
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      fbLoginButtonLoading: true
+    }
+
+    const self = this
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '213151942857648',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v3.0'
+      })
+      FB.AppEvents.logPageView()
+      self.setState({fbLoginButtonLoading: false})
+    }
+
+    this.handleSubmit = (e) => {
+      e.preventDefault()
+      this.props.form.validateFields((err, values) => {
+        if (!err) {
+          var data = new FormData()
+          for (var key in values)
+            data.append(key.toString(), values[key])
+          data.append('csrfmiddlewaretoken', getCookie('csrftoken'))
+          axios.post('/api/signin', data).then((response) => {
+            window.location.href = '/'
+          }).catch(e => {
+            notification['warning']({
+              message: e.response == undefined ? '' : e.response.data,
+              duration: 1.8,
+            })
+          })
+        }
+      })
+    }
+
+    this.facebookLogin = () => {
+      FB.login(function(response) {
+        var data = new FormData()
+        data.append('csrfmiddlewaretoken', getCookie('csrftoken'))
+        data.append('auth_response', JSON.stringify(response.authResponse))
+        axios.post('/api/signin/facebook', data).then(() => {
+          window.location.href = '/'
+        }).catch(e => {
+          notification['warning']({
+            message: e.response == undefined ? '' : e.response.data,
+            duration: 1.8,
+          })
+        })
+      }, {scope: 'email'})
+    }
+
+    this.redirectToNUSSignIn = () => {
+      var host = 'http://' + window.location.host  // TODO: do not hardcode protocol
+      window.location.href =
+        'https://ivle.nus.edu.sg/api/login/?apikey=Z6Q2MnpaPX8sDSOfHTAnN&url=' + host + '/api/signin/nus'
+    }
   }
-  onFailure() {}
+
   componentDidMount() {
     var auth2 = undefined
     function attachSignin(element) {
@@ -31,7 +89,7 @@ class NormalLoginForm extends React.Component {
           data.append('csrfmiddlewaretoken', getCookie('csrftoken'))
           data.append('id_token', googleUser.getAuthResponse().id_token)
           axios.post('/api/signin/google', data).then((response) => {
-            window.location.href = "/"
+            window.location.href = '/'
           }).catch(e => {
             notification['warning']({
               message: e.response == undefined ? '' : e.response.data,
@@ -44,69 +102,20 @@ class NormalLoginForm extends React.Component {
       )
     }
     gapi.load('auth2', function(){
-      // Retrieve the singleton for the GoogleAuth library and set up the client.
       auth2 = gapi.auth2.init({
         client_id: '887521980338-fj0n0r7ui5cn313f4vh6paqm411upf3o.apps.googleusercontent.com',
         cookiepolicy: 'single_host_origin',
       })
       attachSignin(document.getElementById('google-login'))
     })
+  }
 
-    window.fbAsyncInit = function() {
-      FB.init({
-        appId      : '213151942857648',
-        cookie     : true,
-        xfbml      : true,
-        version    : 'v3.0'
-      })
-      FB.AppEvents.logPageView()
-    }
-  }
-  redirectToNUSSignIn() {
-    var host = 'http://' + window.location.host  // TODO: do not hardcode protocol
-    window.location.href =
-      'https://ivle.nus.edu.sg/api/login/?apikey=Z6Q2MnpaPX8sDSOfHTAnN&url=' + host + '/api/signin/nus'
-  }
-  facebookLogin() {
-    FB.login(function(response) {
-      var data = new FormData()
-      data.append('csrfmiddlewaretoken', getCookie('csrftoken'))
-      data.append('auth_response', JSON.stringify(response.authResponse))
-      axios.post('/api/signin/facebook', data).then(() => {
-        window.location.href = "/"
-      }).catch(e => {
-        notification['warning']({
-          message: e.response == undefined ? '' : e.response.data,
-          duration: 1.8,
-        })
-      })
-    }, {scope: 'email'})
-  }
-  handleSubmit = (e) => {
-    e.preventDefault()
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        var data = new FormData()
-        for (var key in values)
-          data.append(key.toString(), values[key])
-        data.append('csrfmiddlewaretoken', getCookie('csrftoken'))
-        axios.post('/api/signin', data).then((response) => {
-          window.location.href = '/'
-        }).catch(e => {
-          notification['warning']({
-            message: e.response == undefined ? '' : e.response.data,
-            duration: 1.8,
-          })
-        })
-      }
-    })
-  }
   render() {
     const { getFieldDecorator } = this.props.form
     return (
       <Row style={{ marginTop: '8%' }}>
         <Col span={8} offset={8}>
-          <Form onSubmit={this.handleSubmit} className="login-form" style={{ margin: 'auto' }}>
+          <Form onSubmit={this.handleSubmit} className='login-form' style={{ margin: 'auto' }}>
             <FormItem
             >
               {getFieldDecorator('email_address', {
@@ -116,26 +125,26 @@ class NormalLoginForm extends React.Component {
                 ],
                 validateTrigger: 'onSubmit'
               })(
-                <Input prefix={<Icon type="mail" style={{ fontSize: 13 }} />} placeholder="Email" />
+                <Input prefix={<Icon type='mail' style={{ fontSize: 13 }} />} placeholder='Email' />
               )}
             </FormItem>
             <FormItem>
               {getFieldDecorator('password', {
                 // rules: [{ required: true, message: 'Please input your Password!' }],
               })(
-                <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="Password" />
+                <Input prefix={<Icon type='lock' style={{ fontSize: 13 }} />} type='password' placeholder='Password' />
               )}
             </FormItem>
             <FormItem>
-              <a className="login-form-forgot" href="">Forgot password</a>
-              <Button type="primary" htmlType="submit" className="login-form-button">
+              <a className='login-form-forgot' href=''>Forgot password</a>
+              <Button type='primary' htmlType='submit' className='login-form-button'>
                 Log in
               </Button>
-              Or <a href="">register now!</a> (coming soon)
+              Or <a href=''>register now!</a> (coming soon)
               <div id='third-party-login' style={{ marginTop: 28 }}>
                 <Button
                   style={{backgroundColor: 'orange', borderColor: 'orange', marginTop: 16, color: 'white'}}
-                  className="login-form-button"
+                  className='login-form-button'
                   htmlType='button'
                   type='primary'
                   onClick={this.redirectToNUSSignIn}
@@ -144,21 +153,22 @@ class NormalLoginForm extends React.Component {
                 </Button>
                 <Button
                   style={{ backgroundColor:'#DD4B39', borderColor:'#DD4B39', marginTop: 16, color: 'white' }}
-                  className="login-form-button"
+                  className='login-form-button'
                   htmlType='button'
                   id='google-login'
                 >
-                  <i className="fa fa-google" aria-hidden="true"></i>
+                  <i className='fa fa-google' aria-hidden='true'></i>
                   {'  '}Log in with Google
                 </Button>
                 <Button
                   style={{ backgroundColor:'#3b5998', borderColor:'#3b5998', marginTop: 16, color: 'white' }}
-                  className="login-form-button"
+                  className='login-form-button'
                   htmlType='button'
                   id='facebook-login'
                   onClick={this.facebookLogin}
+                  loading={this.state.fbLoginButtonLoading}
                 >
-                  <i className="fa fa-facebook-official" aria-hidden="true"></i>
+                  <i className='fa fa-facebook-official' aria-hidden='true'></i>
                   {'  '}Log in with Facebook
                 </Button>
               </div>
