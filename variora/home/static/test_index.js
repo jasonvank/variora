17a16700ffa622ffa5911751dd1234b23ed3b196
royl8 <royl8@qq.com>
2018-07-23 00:54:05 +0800

    ! ADD REDUX to avoid AJAX everytime route to explore page

--- variora/home/static/test_index.js
+++ variora/home/static/test_index.js
@@ -12,16 +12,17 @@ import {
 import { getCookie, getUrlFormat, getValFromUrlParam } from 'util.js'
 
 import { DocumentTab } from './components/document_tab.jsx'
-import { GroupTab } from './components/group_tab.jsx'
 import { ExploreTab } from './components/explore_tab.jsx'
-import { NotificationsToggleButton } from './components/notifications_toggle_button.jsx'
+import { GroupTab } from './components/group_tab.jsx'
 import { NotificationsAlertButton } from './components/notifications_alert_button.jsx'
-
+import { NotificationsToggleButton } from './components/notifications_toggle_button.jsx'
+import {Provider} from 'react-redux'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { SearchResultTab } from './components/search_result_tab.jsx'
 import axios from 'axios'
 import enUS from 'antd/lib/locale-provider/en_US'
+import {store} from './redux/store.js'
 
 const FormItem = Form.Item
 
@@ -149,101 +150,103 @@ class App extends React.Component {
   render() {
     const fields = this.state.fields
     return (
-      <Layout style={{ height: '100%', width: '100%', position: 'absolute' }}>
-        <Header className="header" style={{ backgroundColor: '#fff', diplay: 'inline' }}>
-          <Row>
-            <Col span={6}>
-              {/* <div className="logo" /> */}
-              <img src="/media/logo.png" height={48} style={{ verticalAlign: 'middle', marginLeft: 28 }}/>
-            </Col>
-            <Col span={8} style={{ textAlign: 'right' }}>
-              <Search
-                placeholder="input search text"
-                style={{ width: '60%' }}
-                onSearch={this.handleSearch}
-                defaultValue={window.location.pathname == '/search' ? getValFromUrlParam('key') : '' }
-              />
-            </Col>
-            <Col span={10} style={{ textAlign: 'right' }}>
-              <NotificationsToggleButton user={ this.state.user } acceptInvitationCallback={ this.acceptInvitationCallback } />
-              <NotificationsAlertButton />
-              <span style={{ marginRight: 12, marginLeft: 28, color: '#666' }}>{ this.state.user.nickname }</span>
-              { this.state.user.is_authenticated ? <a onClick={this.signOff}>sign off</a> : <a href="/sign-in">sign in</a> }
-              <Avatar
-                style={{ marginLeft: 28, marginRight: 18, marginTop: -2, verticalAlign: 'middle' }}
-                size={'large'}
-                src={this.state.user.portrait_url}
-              />
-            </Col>
-          </Row>
-        </Header>
+      <Provider store={store}>
+        <Layout style={{ height: '100%', width: '100%', position: 'absolute' }}>
+          <Header className="header" style={{ backgroundColor: '#fff', diplay: 'inline' }}>
+            <Row>
+              <Col span={6}>
+                {/* <div className="logo" /> */}
+                <a href='/'><img src="/media/logo.png" height={48} style={{ verticalAlign: 'middle', marginLeft: 28 }}/></a>
+              </Col>
+              <Col span={8} style={{ textAlign: 'right' }}>
+                <Search
+                  placeholder="input search text"
+                  style={{ width: '60%' }}
+                  onSearch={this.handleSearch}
+                  defaultValue={window.location.pathname == '/search' ? getValFromUrlParam('key') : '' }
+                />
+              </Col>
+              <Col span={10} style={{ textAlign: 'right' }}>
+                <NotificationsToggleButton user={ this.state.user } acceptInvitationCallback={ this.acceptInvitationCallback } />
+                <NotificationsAlertButton />
+                <span style={{ marginRight: 12, marginLeft: 28, color: '#666' }}>{ this.state.user.nickname }</span>
+                { this.state.user.is_authenticated ? <a onClick={this.signOff}>sign off</a> : <a href="/sign-in">sign in</a> }
+                <Avatar
+                  style={{ marginLeft: 28, marginRight: 18, marginTop: -2, verticalAlign: 'middle' }}
+                  size={'large'}
+                  src={this.state.user.portrait_url}
+                />
+              </Col>
+            </Row>
+          </Header>
 
-        <Router basename={URL_BASE}>
-          <Layout>
-            <Sider className='sider' width={200} style={{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0 }}>
-              <Menu
-                mode="inline"
-                defaultOpenKeys={['admin_teams', 'member_teams']}
-                onClick={this.onClickCreateGroupMenuItem}
-                style={{ height: '100%', borderRight: 0 }}
-                defaultSelectedKeys={this.getHighlightedMenuItems()}
-              >
-                <Menu.Item key="explore" >
-                  <Link to="/explore"><span><Icon type="compass" />explore</span></Link>
-                </Menu.Item>
-                <Menu.Item key="documents" disabled={!this.state.user.is_authenticated}>
-                  <Link to="/"><span><Icon type='file' />documents</span></Link>
-                </Menu.Item>
-                <SubMenu key="admin_teams" title={<span><Icon type="solution" />admin group</span>} disabled={!this.state.user.is_authenticated}>
-                  {
-                    this.state.administratedCoteries.map((coterie) => {
-                      return (
-                        <Menu.Item key={'groups' + coterie.pk}>
-                          <Link to={ '/groups/' + coterie.pk }><span>{ coterie.name }</span></Link>
-                        </Menu.Item>
-                      )
-                    })
-                  }
-                  <Menu.Item disabled={!this.state.user.is_authenticated} key={CREATE_NEW_GROUP_MENU_ITEM_KEY}><Icon type="plus"/></Menu.Item>
-                </SubMenu>
-                <SubMenu key="member_teams" title={<span><Icon type="team" />member group</span>} disabled={!this.state.user.is_authenticated}>
-                  {
-                    this.state.joinedCoteries.map((coterie) => {
-                      return (
-                        <Menu.Item key={'groups' + coterie.pk}>
-                          <Link to={ '/groups/' + coterie.pk }><span>{ coterie.name }</span></Link>
-                        </Menu.Item>
-                      )
-                    })
-                  }
-                </SubMenu>
-                <Modal
-                  title="create a new group"
-                  wrapClassName="vertical-center-modal"
-                  visible={this.state.createGroupModelVisible}
-                  onOk={this.submitCreateCoterieForm}
-                  onCancel={() => this.setCreateGroupModelVisible(false)}
+          <Router basename={URL_BASE}>
+            <Layout>
+              <Sider className='sider' width={200} style={{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0 }}>
+                <Menu
+                  mode="inline"
+                  defaultOpenKeys={['admin_teams', 'member_teams']}
+                  onClick={this.onClickCreateGroupMenuItem}
+                  style={{ height: '100%', borderRight: 0 }}
+                  defaultSelectedKeys={this.getHighlightedMenuItems()}
                 >
-                  <CustomizedForm {...fields} onChange={this.handleCreateCoterieFromChange} />
-                </Modal>
-              </Menu>
-            </Sider>
-            <Layout style={{ marginLeft: 200, padding: 0 }}>
-              <Content>
-                <Switch>
-                  <Route exact path="/explore" component={ExploreTab} />
-                  <Route path="/search" component={SearchResultTab} />
-                  <Route path="/groups/:coteriePk" render={ ({match, location}) => this.renderGroupTab(match, location) } />
-                  <Route path="/" component={DocumentTab} />
-                </Switch>
-              </Content>
-              <Footer style={{ textAlign: 'center' }}>
-                  © 2018 Variora. Reach us via <a style={{ color: '#37b' }} href='mailto:variora@outlook.com'>variora@outlook.com</a>
-              </Footer>
+                  <Menu.Item key="explore" >
+                    <Link to="/explore"><span><Icon type="compass" />explore</span></Link>
+                  </Menu.Item>
+                  <Menu.Item key="documents" disabled={!this.state.user.is_authenticated}>
+                    <Link to="/"><span><Icon type='file' />documents</span></Link>
+                  </Menu.Item>
+                  <SubMenu key="admin_teams" title={<span><Icon type="solution" />admin group</span>} disabled={!this.state.user.is_authenticated}>
+                    {
+                      this.state.administratedCoteries.map((coterie) => {
+                        return (
+                          <Menu.Item key={'groups' + coterie.pk}>
+                            <Link to={ '/groups/' + coterie.pk }><span>{ coterie.name }</span></Link>
+                          </Menu.Item>
+                        )
+                      })
+                    }
+                    <Menu.Item disabled={!this.state.user.is_authenticated} key={CREATE_NEW_GROUP_MENU_ITEM_KEY}><Icon type="plus"/></Menu.Item>
+                  </SubMenu>
+                  <SubMenu key="member_teams" title={<span><Icon type="team" />member group</span>} disabled={!this.state.user.is_authenticated}>
+                    {
+                      this.state.joinedCoteries.map((coterie) => {
+                        return (
+                          <Menu.Item key={'groups' + coterie.pk}>
+                            <Link to={ '/groups/' + coterie.pk }><span>{ coterie.name }</span></Link>
+                          </Menu.Item>
+                        )
+                      })
+                    }
+                  </SubMenu>
+                  <Modal
+                    title="create a new group"
+                    wrapClassName="vertical-center-modal"
+                    visible={this.state.createGroupModelVisible}
+                    onOk={this.submitCreateCoterieForm}
+                    onCancel={() => this.setCreateGroupModelVisible(false)}
+                  >
+                    <CustomizedForm {...fields} onChange={this.handleCreateCoterieFromChange} />
+                  </Modal>
+                </Menu>
+              </Sider>
+              <Layout style={{ marginLeft: 200, padding: 0 }}>
+                <Content>
+                  <Switch>
+                    <Route exact path="/explore" component={ExploreTab} />
+                    <Route path="/search" component={SearchResultTab} />
+                    <Route path="/groups/:coteriePk" render={ ({match, location}) => this.renderGroupTab(match, location) } />
+                    <Route path="/" component={DocumentTab} />
+                  </Switch>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>
+                    © 2018 Variora. Reach us via <a style={{ color: '#37b' }} href='mailto:variora@outlook.com'>variora@outlook.com</a>
+                </Footer>
+              </Layout>
             </Layout>
-          </Layout>
-        </Router>
-      </Layout>
+          </Router>
+        </Layout>
+      </Provider>
     )
   }
 }
