0a1bcfe172b80a3ea06c15ba96c9349d0116c112
jasonvank <fwenkai.jason@gmail.com>
2018-10-13 13:59:47 +0800

    Replace header with the original header

--- variora/home/static/jason_test_index.js
+++ variora/home/static/jason_test_index.js
@@ -16,6 +16,7 @@ import { initialStore } from './redux/init_store.js'
 import TextArea from '../../../node_modules/antd/lib/input/TextArea'
 
 import DocumentTitle from 'react-document-title';
+import { NotificationsAlertButton } from './components/notifications_alert_button.jsx'
 import { enquireScreen } from 'enquire-js';
 import { HeaderPage } from './components/landing_page/Header.jsx';
 import { Banner } from './components/landing_page/Banner.jsx';
@@ -27,7 +28,8 @@ const FormItem = Form.Item
 import './components/landing_page/static/style';
 
 const CREATE_NEW_GROUP_MENU_ITEM_KEY = 'createGroupButton'
-
+const { Header } = Layout
+const Search = Input.Search
 const URL_BASE = ''
 
 class HomeBeforeConnect extends React.Component {
@@ -123,7 +125,39 @@ class HomeBeforeConnect extends React.Component {
   render() {
     return (
       <div>
-        <HeaderPage key="header" isFirstScreen={this.state.isFirstScreen} />
+        <Header className="header" style={{ backgroundColor: '#fff', diplay: 'inline' }}>
+          <Row>
+            <Col span={6}>
+              <a href='/'><img src="/media/logo.png" height={48} style={{ verticalAlign: 'middle', marginLeft: 28 }}/></a>
+            </Col>
+            <Col span={8} style={{ textAlign: 'right' }}>
+              <Search
+                placeholder="input search text"
+                style={{ width: '60%' }}
+                onSearch={this.handleSearch}
+                defaultValue={window.location.pathname.includes('/search') ? getValFromUrlParam('key') : '' }
+              />
+            </Col>
+            <Col span={10} style={{ textAlign: 'right' }}>
+              {/* <NotificationsToggleButton user={ this.state.user } acceptInvitationCallback={ this.acceptInvitationCallback } /> */}
+              <NotificationsAlertButton />
+              <Icon type="team"
+                onClick={() => {
+                  notification['info']({message: 'We are rewriting the group function, it will come soon.'})
+                }}
+                style={{ fontSize: 18, marginLeft: 28, verticalAlign: 'middle' }}
+              />
+              <span style={{ marginRight: 12, marginLeft: 28, color: '#666' }}>{ this.state.user.nickname }</span>
+              { this.state.user.is_authenticated ? <a onClick={this.signOff}>Sign Off</a> : <a href="/sign-in">sign in</a> }
+              <Avatar
+                style={{ marginLeft: 28, marginRight: 18, marginTop: -2, verticalAlign: 'middle' }}
+                size={'large'}
+                src={this.state.user.portrait_url}
+              />
+            </Col>
+          </Row>
+        </Header>
+
         <Banner key="banner" onEnterChange={this.onEnterChange} />
         <Page1 key="page1" />
         <Page2 key="page2" />
