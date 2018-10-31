260958684917faaada9dedc5a689ded260244de4
royl8 <royl8@qq.com>
2018-10-31 22:04:54 +0800

    fix domain bug

--- variora/home/cron.py
+++ variora/home/cron.py
@@ -91,7 +91,7 @@ def _get_unread_notification_list(user):
 
     context = {
         'notification_list': [NotificationEncoder().default(notif) for notif in unread_notifications],
-        'domain': 'https://variora.io'
+        'domain': 'https://www.variora.io'
     }
     return context
 
