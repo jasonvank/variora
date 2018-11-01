511b071048be7a715312e0fe97e52f495f0f2305
royl8 <royl8@qq.com>
2018-11-01 23:17:31 +0800

    apply to group feature

--- variora/home/api/views.py
+++ variora/home/api/views.py
@@ -43,8 +43,8 @@ class CombinedEncoder(DjangoJSONEncoder):
             return DocumentEncoder().default(obj)
         elif isinstance(obj, Readlist):
             return ReadlistListEncoder().default(obj)
-        # elif isinstance(obj, Coterie):
-        #     return SearchResultCoterieEncoder().default(obj)
+        elif isinstance(obj, Coterie):
+            return SearchResultCoterieEncoder().default(obj)
         else:
             return super(CombinedEncoder, self).default(obj)
 
@@ -57,13 +57,13 @@ def search_api_view(request):
 
     result_documents = list(Document.objects.filter_with_related(title__icontains=key))[:100]  # case-insensitive contain
     result_users = list(User.objects.filter(Q(nickname__icontains=key) | Q(email_address__icontains=key)))[:100]
-    # result_coteries = list(Coterie.objects.filter(Q(name__icontains=key) | Q(id__icontains=key)))[:100]
+    result_coteries = list(Coterie.objects.filter(Q(name__icontains=key) | Q(id__icontains=key)))[:100]
     result_readlists = list(Readlist.objects.filter(Q(name__icontains=key) | Q(description__icontains=key)).filter(is_public=True))[:100]
     return JsonResponse(
         {
             'resultDocuments': result_documents,
             'resultUsers': result_users,
-            # 'resultCoteries': result_coteries,
+            'resultCoteries': result_coteries,
             'resultReadlists': result_readlists,
         },
         encoder=CombinedEncoder,
--- variora/home/static/components/search_result_tab/search_result_tab.jsx
+++ variora/home/static/components/search_result_tab/search_result_tab.jsx
@@ -1,6 +1,6 @@
 import 'regenerator-runtime/runtime'
 
-import { Avatar, Icon, Layout, Menu, Table, notification } from 'antd'
+import { Avatar, Icon, Layout, Menu, Table, notification, Modal, Input, Tooltip } from 'antd'
 import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 
 import { DocumentResult } from './document_result_subtab.jsx'
@@ -11,10 +11,12 @@ import axios from 'axios'
 import { getUrlFormat } from 'util.js'
 
 const { Content } = Layout
+const { TextArea } = Input
 
-var fullUrl = window.location.href
+
+const fullUrl = window.location.href
 // TODO: refactoring this
-var searchKey = fullUrl.slice(fullUrl.indexOf('=') + 1)
+const searchKey = fullUrl.slice(fullUrl.indexOf('=') + 1)
 
 class SearchResultTab extends React.Component {
   constructor(props) {
@@ -47,10 +49,10 @@ class SearchResultTab extends React.Component {
     axios.get(getUrlFormat('/api/search', {
       'key': searchKey
     })).then((response) => {
-      var data = response.data
+      const data = response.data
       this.setState({
         resultDocuments: data.resultDocuments,
-        // resultCoteries: data.resultCoteries,
+        resultCoteries: data.resultCoteries,
         resultUsers: data.resultUsers,
         resultReadlists: data.resultReadlists,
       })
@@ -85,6 +87,9 @@ class SearchResultTab extends React.Component {
           <Menu.Item key='search-users'>
             <Link to={urlBase + '/users?key=' + searchKey}><Icon type='user' />Users</Link>
           </Menu.Item>
+          <Menu.Item key='search-groups'>
+            <Link to={urlBase + '/groups?key=' + searchKey}><Icon type='team' />Groups</Link>
+          </Menu.Item>
         </Menu>
 
         <Switch>
@@ -105,6 +110,12 @@ class SearchResultTab extends React.Component {
               resultReadlists={this.state.resultReadlists}
             />}
           />
+
+          <Route path={urlBase + '/groups'} render={() =>
+            <GroupResult
+              administratedCoteries={this.state.administratedCoteries} joinedCoteries={this.state.joinedCoteries} resultCoteries={this.state.resultCoteries} user={this.state.user}
+            />}
+          />
         </Switch>
       </Content>
     )
@@ -112,120 +123,120 @@ class SearchResultTab extends React.Component {
 }
 
 
-// class GroupResult extends React.Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//       sortedInfo: null,
-//       data: this.props.resultCoteries,
-//       use: this.props.user,
-//       createApplicationModelVisible: false,
-//       targetedCoterie: {name: ''},
-//       applicationMessage: ''
-//     }
-//     this.handleChange = (sorter) => {
-//       this.setState({
-//         sortedInfo: sorter,
-//       })
-//     }
-//     this.onApplyClick = (coterie) => {
-//       this.setState({
-//         createApplicationModelVisible: true,
-//         targetedCoterie: coterie,
-//       })
-//     }
-//     this.submitApplicationCoterieForm = () => {
-//       var data = new FormData()
-//       data.append('coterie_id', this.state.targetedCoterie.pk)
-//       data.append('application_message', this.state.applicationMessage)
-//       data.append('csrfmiddlewaretoken', getCookie('csrftoken'))
-//       axios.post('/coterie/api/apply', data)
-//       .then((response) => {
-//         this.setState({
-//           applicationMessage: this.state.applicationMessage,
-//           createApplicationModelVisible: false,
-//         })
-//         notification['success']({
-//           message: 'Application successfully sent',
-//           description: 'Your application has been sent to Group: ' + this.state.targetedCoterie.name + ' successfully!' + ' With message: ' + this.state.applicationMessage,
-//           duration: 4
-//         })
-//       })
-//     }
-//   }
-//
-//   componentWillReceiveProps(nextProps) {
-//     this.setState({
-//       data: nextProps.resultCoteries,
-//       user: nextProps.user,
-//     })
-//   }
-//
-//   render() {
-//     let sortedInfo = this.state.sortedInfo
-//     sortedInfo = sortedInfo || {}
-//
-//     const columns = [{
-//       title: 'Group Name',
-//       dataIndex: 'name',
-//       width: '40%',
-//       sorter: (a, b) => a.name.localeCompare(b.name),
-//     }, {
-//       title: "Admin's Email",
-//       dataIndex: '',
-//       width: '30%',
-//       render: (text, record) => record.administrators.length == 0 ? '' : record.administrators[0].email_address
-//     }, {
-//       title: 'Action',
-//       key: 'action',
-//       width: '30%',
-//       render: (text, record) => {
-//         var applyLink = (
-//           <a href='javascript:;' onClick={() => this.onApplyClick(record)}>Apply</a>
-//         )
-//         var alreadyMemberLink = (
-//           <Tooltip placement='right' title={'You are a member of this group'}>
-//             <span><a disabled='disabled'>Apply</a></span>
-//           </Tooltip>
-//         )
-//         var alreadyAdminLink = (
-//           <Tooltip placement='right' title={'You are a admin of this group'}>
-//             <span><a disabled='disabled'>Apply</a></span>
-//           </Tooltip>
-//         )
-//         var isMember = this.props.joinedCoteries.map(c => c.pk).includes(record.pk)
-//         var isAdmin = this.props.administratedCoteries.map(c => c.pk).includes(record.pk)
-//         return isAdmin ? alreadyAdminLink : isMember ? alreadyMemberLink : applyLink
-//       }
-//     }]
-//
-//     return (
-//       <div>
-//         <Table
-//           dataSource={this.state.data}
-//           columns={columns}
-//           className={'card'}
-//           style={{ overflow: 'auto', backgroundColor: 'white', marginTop: 18, padding: 18 }}
-//           pagination={{ pageSize: 10 }}
-//           rowKey={record => record.pk}
-//           onChange={this.handleChange}
-//         />
-//         <Modal
-//           title={'Apply to join: ' + this.state.targetedCoterie.name}
-//           wrapClassName='vertical-center-modal'
-//           visible={this.state.createApplicationModelVisible}
-//           onOk={this.submitApplicationCoterieForm}
-//           onCancel={() => {this.setState({ createApplicationModelVisible: false })}}
-//         >
-//           <TextArea
-//             onChange={async (e) => this.setState({ applicationMessage: e.target.value })}
-//             value={this.state.applicationMessage}
-//           ></TextArea>
-//         </Modal>
-//       </div>
-//     )
-//   }
-// }
+class GroupResult extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      sortedInfo: null,
+      data: this.props.resultCoteries,
+      use: this.props.user,
+      createApplicationModelVisible: false,
+      targetedCoterie: {name: ''},
+      applicationMessage: ''
+    }
+    this.handleChange = (sorter) => {
+      this.setState({
+        sortedInfo: sorter,
+      })
+    }
+    this.onApplyClick = (coterie) => {
+      this.setState({
+        createApplicationModelVisible: true,
+        targetedCoterie: coterie,
+      })
+    }
+    this.submitApplicationCoterieForm = () => {
+      var data = new FormData()
+      data.append('coterie_id', this.state.targetedCoterie.pk)
+      data.append('application_message', this.state.applicationMessage)
+      data.append('csrfmiddlewaretoken', getCookie('csrftoken'))
+      axios.post('/coterie/api/apply', data)
+      .then((response) => {
+        this.setState({
+          applicationMessage: this.state.applicationMessage,
+          createApplicationModelVisible: false,
+        })
+        notification['success']({
+          message: 'Application successfully sent',
+          description: 'Your application has been sent to Group: ' + this.state.targetedCoterie.name + ' successfully!' + ' With message: ' + this.state.applicationMessage,
+          duration: 4
+        })
+      })
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      data: nextProps.resultCoteries,
+      user: nextProps.user,
+    })
+  }
+
+  render() {
+    let sortedInfo = this.state.sortedInfo
+    sortedInfo = sortedInfo || {}
+
+    const columns = [{
+      title: 'Group Name',
+      dataIndex: 'name',
+      width: '40%',
+      sorter: (a, b) => a.name.localeCompare(b.name),
+    }, {
+      title: "Admin's Email",
+      dataIndex: '',
+      width: '30%',
+      render: (text, record) => record.administrators.length == 0 ? '' : record.administrators[0].email_address
+    }, {
+      title: 'Action',
+      key: 'action',
+      width: '30%',
+      render: (text, record) => {
+        var applyLink = (
+          <a href='javascript:;' onClick={() => this.onApplyClick(record)}>Apply</a>
+        )
+        var alreadyMemberLink = (
+          <Tooltip placement='right' title={'You are a member of this group'}>
+            <span><a disabled='disabled'>Apply</a></span>
+          </Tooltip>
+        )
+        var alreadyAdminLink = (
+          <Tooltip placement='right' title={'You are a admin of this group'}>
+            <span><a disabled='disabled'>Apply</a></span>
+          </Tooltip>
+        )
+        var isMember = this.props.joinedCoteries.map(c => c.pk).includes(record.pk)
+        var isAdmin = this.props.administratedCoteries.map(c => c.pk).includes(record.pk)
+        return isAdmin ? alreadyAdminLink : isMember ? alreadyMemberLink : applyLink
+      }
+    }]
+
+    return (
+      <div>
+        <Table
+          dataSource={this.state.data}
+          columns={columns}
+          className={'card'}
+          style={{ overflow: 'auto', backgroundColor: 'white', marginTop: 18, padding: 18 }}
+          pagination={{ pageSize: 10 }}
+          rowKey={record => record.pk}
+          onChange={this.handleChange}
+        />
+        <Modal
+          title={'Apply to join: ' + this.state.targetedCoterie.name}
+          wrapClassName='vertical-center-modal'
+          visible={this.state.createApplicationModelVisible}
+          onOk={this.submitApplicationCoterieForm}
+          onCancel={() => {this.setState({ createApplicationModelVisible: false })}}
+        >
+          <TextArea
+            onChange={async (e) => this.setState({ applicationMessage: e.target.value })}
+            value={this.state.applicationMessage}
+          ></TextArea>
+        </Modal>
+      </div>
+    )
+  }
+}
 
 class UserResult extends React.Component {
   constructor(props) {
