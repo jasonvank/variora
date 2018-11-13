47c1ae651ad141ff84fc752f2daee7d314790d6d
royl8 <royl8@qq.com>
2018-11-13 16:16:22 +0800

    add static to the sw cache

--- variora/home/static/service-worker.js
+++ variora/home/static/service-worker.js
@@ -35,40 +35,38 @@ self.addEventListener('activate', event => {
 })
 
 
-// self.addEventListener('fetch', event => {
-//   if (event.request.url.includes('login') || event.request.method != 'GET')
-//     return
-//
-//   event.respondWith(
-//     fetch(event.request)
-//       .then(response => {
-//         const responseClone = response.clone()
-//
-//         if (event.request.url.includes('/static/')
-//             // || event.request.url.includes('/api/')
-//         )
-//           caches
-//             .open(cacheName)
-//             .then(cache => {
-//               cache.put(event.request.url, responseClone)  // online, so update cache on every call
-//             })
-//
-//         return response
-//       })
-//       .catch((res) => {  // cache will be called if offline
-//         return caches
-//                 .open(cacheName).then(cache => cache.match(event.request.url))
-//                 .then(response => {
-//                   if (response) {
-//                     console.log('SW offline: hit  -  ' + event.request.url)
-//                     return response
-//                   } else {
-//                     return res
-//                   }
-//                 })
-//       })
-//   )
-// })
+self.addEventListener('fetch', event => {
+  if (event.request.url.includes('login') || event.request.method != 'GET')
+    return
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const responseClone = response.clone()
+
+        if (event.request.url.includes('/static/'))
+          caches
+            .open(cacheName)
+            .then(cache => {
+              cache.put(event.request.url, responseClone)  // online, so update cache on every call
+            })
+
+        return response
+      })
+      .catch((res) => {  // cache will be called if offline
+        return caches
+          .open(cacheName).then(cache => cache.match(event.request.url))
+          .then(response => {
+            if (response) {
+              console.log('SW offline: hit  -  ' + event.request.url)
+              return response
+            } else {
+              return res
+            }
+          })
+      })
+  )
+})
 
 
 
